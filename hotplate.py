ecda60e161ade6ee33bb42b8f223325f47eb6677
Ken <krrgithub@gmail.com>
2015-09-04 16:15:06 -0600

    Added python versioning.

--- hotplate.py
+++ hotplate.py
@@ -1,10 +1,11 @@
 import png
 import sys
 import itertools
+import fileinput
 
 
-WIDTH = 128
-HEIGHT = 128
+WIDTH = 4096
+HEIGHT = 4096
 
 
 def calc_green(value):
@@ -14,7 +15,7 @@ def calc_blue(value):
     return round(max(50 - value, 0) * (255.0 / 50.0))
 
 def calc_red(value):
-    return round(max(value - 50, 0) * (255.0 / 50.0))
+    return round(value * (255.0 / 100.0))
 
 def calc_rgb(value):
     return calc_red(value), calc_green(value), calc_blue(value)
@@ -27,7 +28,24 @@ def gen_random_row(length):
         map(lambda i: calc_rgb(i * (100.0 / WIDTH)), xrange(WIDTH))
     ))
 
+def stdin_read_heatvalues():
+    for line in fileinput.input():
+        row = map(int, line.split())
+        for value in row:
+            yield value
+
+def parse_heatvalues():
+    array = []
+    iter_heatvalues = stdin_read_heatvalues()
+    for row_i in xrange(HEIGHT):
+        array.append([])
+        for col_j in xrange(WIDTH):
+            array[row_i].extend(calc_rgb(iter_heatvalues.next()))
+    return array
+
+
 # Example gradient heatmap:
-# with open('example_heatmap.png', 'wb') as f:
-#     w = png.Writer(WIDTH, HEIGHT)
-#     w.write(f, [gen_random_row(WIDTH)]*HEIGHT)
+with open('example_heatmap.png', 'wb') as f:
+    w = png.Writer(WIDTH, HEIGHT)
+
+    w.write(f, parse_heatvalues())
