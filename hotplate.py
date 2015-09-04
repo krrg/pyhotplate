ba78479700007b689d6a04a7f15e7c8022367d14
Ken <krrgithub@gmail.com>
2015-09-04 13:04:21 -0600

    Added examples

--- hotplate.py
+++ hotplate.py
@@ -1,7 +1,33 @@
 import png
 import sys
+import itertools
 
-FILENAME = sys.args[1]
-DIMENSION = int(sys.args[2])
 
-map(float, raw_input().strip().split())
+WIDTH = 128
+HEIGHT = 128
+
+
+def calc_green(value):
+    return round((50 - abs(50 - value)) * (255.0 / 50.0))
+
+def calc_blue(value):
+    return round(max(50 - value, 0) * (255.0 / 50.0))
+
+def calc_red(value):
+    return round(max(value - 50, 0) * (255.0 / 50.0))
+
+def calc_rgb(value):
+    return calc_red(value), calc_green(value), calc_blue(value)
+
+def gen_random_pixel():
+    return calc_rgb(randint(0, 100))
+
+def gen_random_row(length):
+    return map(int, itertools.chain(*
+        map(lambda i: calc_rgb(i * (100.0 / WIDTH)), xrange(WIDTH))
+    ))
+
+# Example gradient heatmap:
+# with open('example_heatmap.png', 'wb') as f:
+#     w = png.Writer(WIDTH, HEIGHT)
+#     w.write(f, [gen_random_row(WIDTH)]*HEIGHT)
